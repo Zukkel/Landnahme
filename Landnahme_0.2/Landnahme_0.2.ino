dc893db8d0afb77d0ff4cef49b10563f4d116a89
Kai Kraemer <inf2028@hs-worms.de>
2015-07-31 14:27:43 +0200

    Landnahme_0.4
    Sämtliche Kernfunktionen Implementiert.
    Schrein funktioniert.
    
    TODO: Pin Belegungen eintragen, Demo-Modus, Diagnose-Modus

--- Landnahme_0.2/Landnahme_0.2.ino
+++ Landnahme_0.2/Landnahme_0.2.ino
@@ -50,6 +50,9 @@
 #define LadePin 5
 #define ReadyPin 6
 
+#define real 0
+#define demo 1
+#define diagnostic 2
 
 #define Free 0
 #define activating 1
@@ -81,18 +84,20 @@ Servo myServo;
 #define Zusammenkunft 12
 #define Stadt 13
 #define Dunkel 14
+#define Demo 15
+#define Unbekannt 16
 
 
 const unsigned long ta = 10*minuten;      //Aktivierungszeit, Zeit in Phase 1
-const unsigned long tl = 10*minuten;      //Ladezeit, Zeit in Phase 2
+const unsigned long tl = 60*minuten;      //Ladezeit, Zeit in Phase 2
 const unsigned long tn = 20*minuten;      //Landnahmezeit, Zeit in Phase 3
-const unsigned long tb = 30*minuten;      //Blockzeit, Zeit in Phase 4
+const unsigned long tb = 180*minuten;      //Blockzeit, Zeit in Phase 4
                                           //Phase 0 hat keine Zeit und kann beliebig lange gehen
 
 short _status;
 short lastKeeper;
 short newKeeper;
-
+short mode;
 short servoValue;
 
 unsigned long activeTime;
@@ -103,12 +108,13 @@ int scan;
 
 
 
+
 void setup() {
 Serial.begin(9600);
   myServo.attach(3);
 
   _status=Free;
-  lastKeeper=Neutral;
+
   newKeeper=Neutral;
 
   servoValue = 90;                             //normal 90
@@ -116,6 +122,7 @@ Serial.begin(9600);
 
 
   failCount=0;
+  mode = 0;
 
   activeTime=0;
   _status=0;
@@ -132,31 +139,29 @@ Serial.begin(9600);
 void loop() {
   if(micros()<3*minuten)                                //eine Testphase am anfang. Dauert 3 minuten
   {
-    if(micros()<1000*30)
+    switch(_status)
     {
-      _status = 0;
-    }
-    else if(micros()<1000*90)
-    {
-      _status = 1;
-    }
-    else if(micros()<1000*120)
-    {
-      _status = 2;
-    }
-    else if(micros()<1000*180)
-    {
-      _status = 3;
-    }
-    else if(micros()<1000*290)
-    {
-      _status = 4;
-    }
-    else
-    {
-      _status = 0;
-    }
+      case(activating):
+      
+      break;
+      
+      case(loading):
 
+      break;
+
+      case(active):
+      
+      break;
+
+      case(blocked):
+
+      break;
+
+      default:
+      
+      return;
+
+    }
   }
   else
   {
@@ -181,38 +186,96 @@ void loop() {
       else
       {
         _status++;
+        failCount=0;
         activeTime=micros();
+        setShrine();
       }
       break;
-      
+ 
       case(loading):
+        if((micros()-activeTime)<tl)
+        {
+          scan=scanForCard();
+          if(scan==Lesath||scan==Neutral||scan==Dunkel)
+          {
+
+            newKeeper = scan;
+            _status=0;
+            setShrine();  
+          }
+        }
+        else
+        {
+            _status++;
+            activeTime=micros();
+            setShrine();        
+        }
 
       break;
 
       case(active):
+      if((micros()-activeTime)<tn)
+      {
+        scan=scanForCard();
+        if(scan==Lesath||scan==Neutral||scan==Dunkel)
+        {
+          newKeeper = scan;
+          _status=0;
+          setShrine();  
+        }
+        else if(scan!=Unbekannt)
+        {
+          newKeeper=scan;
+          _status++;
+          setServo(newKeeper);
+          setShrine();
+          activeTime=micros();          
+        }
+        
+      }
+      else
+      {
+        _status=0;
+        setServo(newKeeper);
+        setShrine();
+        activeTime=micros();
+      }
       
       break;
 
       case(blocked):
-
+      if((micros()-activeTime)<tb)
+      {
+        scan=scanForCard();
+        if(scan==Lesath||scan==Neutral||scan==Dunkel)
+        {
+          newKeeper = scan;
+          _status=0;
+          activeTime=micros();
+          setShrine();  
+        }
+      }
+      else
+      {
+        _status=0;
+        activeTime=micros();
+        setShrine();
+      }
       break;
 
       default:
         scan=scanForCard();
-        if(scan==0||scan==7||scan==14)
+        if(scan==Lesath||scan==Neutral||scan==Dunkel)
         {
-          lastKeeper = scan;
           newKeeper = scan;
-
-          myServo.write(12*scan);
-          delay(15);
-          myServo.write(myServo.read());
+          setShrine();
         }
-        else if(scan!=15)
+        else if(scan!=Unbekannt)
         {
           _status++;
           failCount=0;
           activeTime=micros();
+          setShrine();
         }
       
       return;
@@ -226,7 +289,7 @@ void loop() {
 int scanForCard()
 {
    if (!mfrc522.PICC_IsNewCardPresent())
-    return 15;                                                            //15 bedeutet das keine karte gefunden wurde
+    return Unbekannt;                                                            //Unbekannt bedeutet das keine karte gefunden wurde = 16
    else
    {
     String rfidUid = "";
@@ -235,36 +298,50 @@ int scanForCard()
       rfidUid += String(mfrc522.uid.uidByte[i], HEX);
     }
 
-    if(rfidUid == "")
-      return 0;
-    if(rfidUid == "")
-      return 1;
-    if(rfidUid == "")
-      return 2;
-    if(rfidUid == "")
-      return 3;
-    if(rfidUid == "")
-      return 4;
-    if(rfidUid == "")
-      return 5;
-    if(rfidUid == "")
-      return 6;
-    if(rfidUid == "")
-      return 7;
-    if(rfidUid == "")
-      return 8;
-    if(rfidUid == "")
-      return 9;
-    if(rfidUid == "")
-      return 10;
-    if(rfidUid == "")
-      return 11;
-    if(rfidUid == "")
-      return 12;
-    if(rfidUid == "")
-      return 13;
-    if(rfidUid == "")
-      return 14;
+    if(rfidUid == "000704343d")
+      return Lesath;
+    if(rfidUid == "0002033c00")
+      return Antike;
+    if(rfidUid == "000704361a")
+      return Elben;
+    if(rfidUid == "0007050246")
+      return Komet;
+    if(rfidUid == "00070b1929")
+      return HDC;
+    if(rfidUid == "000661214d")
+      return Imperium;
+    if(rfidUid == "0007050455")
+      return Krone;
+    if(rfidUid == "000704343d")
+      return Neutral;
+    if(rfidUid == "0007205d05")
+      return Licht;
+    if(rfidUid == "0006613b3d")
+      return Norrelag;
+    if(rfidUid == "0007092c5a")
+      return OHL;
+    if(rfidUid == "0006540c50")
+      return Pilger;
+    if(rfidUid == "0002034400")
+      return Zusammenkunft;
+    if(rfidUid == "0006531113")
+      return Stadt;
+    if(rfidUid == "0007050456")
+      return Dunkel;
+    if(rfidUid == "0006542540")
+      return Demo;
   }
+}
+
+void setServo(int Lager)
+{
+
+  return;
+}
+
+void setShrine()
+{
+
+  return;
 }
 
