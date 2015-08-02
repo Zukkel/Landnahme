c0e5e7342d5033d039ae7a3c7c97871c5837a122
Kai Kraemer <inf2028@hs-worms.de>
2015-08-03 00:01:54 +0200

    Landnahme_v0.7
    ShiftPWM raus, alles mit standart neu......

--- Landnahme/Landnahme.ino
+++ Landnahme/Landnahme.ino
@@ -27,65 +27,6 @@
  */
 
 
-
-
-
-
-// ShiftPWM uses timer1 by default. To use a different timer, before '#include <ShiftPWM.h>', add
-#define SHIFTPWM_USE_TIMER2  // for Arduino Uno and earlier (Atmega328)
-// #define SHIFTPWM_USE_TIMER3  // for Arduino Micro/Leonardo (Atmega32u4)
-
-// Clock and data pins are pins from the hardware SPI, you cannot choose them yourself.
-// Data pin is MOSI (Uno and earlier: 11, Leonardo: ICSP 4, Mega: 51, Teensy 2.0: 2, Teensy 2.0++: 22) 
-// Clock pin is SCK (Uno and earlier: 13, Leonardo: ICSP 3, Mega: 52, Teensy 2.0: 1, Teensy 2.0++: 21)
-
-// You can choose the latch pin yourself.
-const int ShiftPWM_latchPin=0;
-
-
-#define SHIFTPWM_NOSPI
-const int ShiftPWM_dataPin = 1;
-const int ShiftPWM_clockPin = 2;
-
-
-// If your LED's turn on if the pin is low, set this to true, otherwise set it to false.
-const bool ShiftPWM_invertOutputs = false;
-
-// You can enable the option below to shift the PWM phase of each shift register by 8 compared to the previous.
-// This will slightly increase the interrupt load, but will prevent all PWM signals from becoming high at the same time.
-// This will be a bit easier on your power supply, because the current peaks are distributed.
-const bool ShiftPWM_balanceLoad = false;
-
-#include <ShiftPWM.h>   // include ShiftPWM.h after setting the pins!
-
-// Function prototypes (telling the compiler these functions exist).
-void oneByOne(void);
-void inOutTwoLeds(void);
-void inOutAll(void);
-void alternatingColors(void);
-void hueShiftAll(void);
-void randomColors(void);
-void fakeVuMeter(void);
-void rgbLedRainbow(unsigned long cycleTime, int rainbowWidth);
-void printInstructions(void);
-
-// Here you set the number of brightness levels, the update frequency and the number of shift registers.
-// These values affect the load of ShiftPWM.
-// Choose them wisely and use the PrintInterruptLoad() function to verify your load.
-unsigned char maxBrightness = 255;
-unsigned char pwmFrequency = 75;
-unsigned int numRegisters = 1;
-unsigned int numOutputs = 6;
-unsigned int numRGBLeds = 2;
-unsigned int fadingMode = 0; //start with all LED's off.
-
-unsigned long startTime = 0; // start time for the chosen fading mode
-
-
- 
-
-
-
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Servo.h>
@@ -107,11 +48,14 @@ unsigned long startTime = 0; // start time for the chosen fading mode
 
 
 //pin belegungen. kann je nach aufbau verändert werden.
-
-#define servoLED1_PIN1 3
-#define servoLED1_PIN2 4
-#define servoLED2_PIN1 5
-#define servoLED2_PIN2 6
+#define rgb_red_PIN1 0
+#define rgb_red_PIN2 1
+#define rgb_green_PIN1 2
+#define rgb_green_PIN2 3
+#define rgb_blue_PIN1 4
+#define rgb_blue_PIN2 5
+#define servoLED1_PIN 6
+#define servoLED2_PIN 7
 #define servo_PIN 8
 #define RST_PIN 9
 #define SS_PIN 10
@@ -170,29 +114,22 @@ int scan;
 
 void setup() {
 Serial.begin(9600);
-  myServo.attach(servo_PIN);
-  pinMode(servoLED1_PIN1, OUTPUT);
-  pinMode(servoLED1_PIN2, OUTPUT);
-
-    // Sets the number of 8-bit registers that are used.
-  ShiftPWM.SetAmountOfRegisters(numRegisters);
-
-  // SetPinGrouping allows flexibility in LED setup. 
-  // If your LED's are connected like this: RRRRGGGGBBBBRRRRGGGGBBBB, use SetPinGrouping(4).
-  ShiftPWM.SetPinGrouping(2); //This is the default, but I added here to demonstrate how to use the funtion
-  
-  ShiftPWM.Start(pwmFrequency,maxBrightness);  
+  pinMode(servoLED1_PIN, OUTPUT);
+  pinMode(servoLED2_PIN, OUTPUT);
+  pinMode(rgb_red_PIN1,OUTPUT);
+  pinMode(rgb_red_PIN2,OUTPUT);
+  pinMode(rgb_blue_PIN1,OUTPUT);
+  pinMode(rgb_blue_PIN2,OUTPUT);
+  pinMode(rgb_green_PIN1,OUTPUT);
+  pinMode(rgb_green_PIN2,OUTPUT);
 
   _status=Free;
   newKeeper=Neutral;
-
-  servoValue = 90;                             //normal 90
+  mode = real;
+  
   servoDirection=1;
-  myServo.write(servoValue);
-
 
   failCount=0;
-  mode = 0;
 
   activeTime=0;
 
@@ -202,7 +139,6 @@ Serial.begin(9600);
   mfrc522.PCD_Init();
 
   setShrine();
-
 }
 
 void loop() {
@@ -233,17 +169,20 @@ void loop() {
         failCount=0;
         activeTime=micros();
         setShrine();
+        myServo.attach(8);
       }
       break;
  
       case(loading):
+      delay(100);
       pulse();
         if((micros()-activeTime)<tl)
         {
           scan=scanForCard();
           if(scan==Lesath||scan==Neutral||scan==Dunkel)
           {
-
+            myServo.detach();
+            delay(3000);
             newKeeper = scan;
             _status=0;
             setShrine();  
@@ -265,12 +204,16 @@ void loop() {
         scan=scanForCard();
         if(scan==Lesath||scan==Neutral||scan==Dunkel)
         {
+          myServo.detach();
+          delay(3000);
           newKeeper = scan;
           _status=0;
           setShrine();  
         }
-        else if(scan!=Unbekannt)
+        else if((scan!=Unbekannt)&&(scan!=Demo)&&(scan!=Diagnostic))
         {
+          myServo.detach();
+          delay(3000);
           newKeeper=scan;
           _status++;
           setShrine();
@@ -280,6 +223,8 @@ void loop() {
       }
       else
       {
+        myServo.detach();
+        delay(3000);
         _status=0;
         setShrine();
         activeTime=micros();
@@ -314,11 +259,11 @@ void loop() {
           newKeeper = scan;
           setShrine();
         }
-        else if(scan==demo)
+        else if(scan==Demo)
         {
           mode=demo;
         }
-        else if(scan==diagnostic)
+        else if(scan==Diagnostic)
         {
           mode=diagnostic;
         }
@@ -408,6 +353,7 @@ int scanForCard()
 void setServo()
 {
   myServo.write(7.5+15*newKeeper);
+  delay(3000);
 }
 
 void setShrine()
@@ -416,127 +362,90 @@ void setShrine()
   switch(_status)
   {
     case(activating):
-
-    //RGB AUF BLAU
-    ShiftPWM.SetRGB(1,0,0,255);    // blue
-    ShiftPWM.SetRGB(2,0,0,255);    // blue
-    
-
-    
-    if(newKeeper==Lesath||newKeeper==Neutral||newKeeper==Dunkel)
-    {
-      //WENN SL-LAGER: SERVO-LED AUS
-      digitalWrite(servoLED1_PIN1, LOW);
-      digitalWrite(servoLED1_PIN2, LOW);
-
-      digitalWrite(servoLED2_PIN1, HIGH);
-      digitalWrite(servoLED2_PIN2, HIGH);      
-      
-      switch(newKeeper)
-      {
-        case(Lesath):
-        myServo.write(30);
-        break;
-        case(Neutral):
-        myServo.write(90);
-
-        break;
-        case(Dunkel):
-        myServo.write(150);
-
-        break;
-        
-      }
-    }
-    else
-    {
-
-      //SERVO LEDs AN
-      digitalWrite(servoLED1_PIN1, HIGH);
-      digitalWrite(servoLED1_PIN2, HIGH);
-
-      digitalWrite(servoLED2_PIN1, LOW);
-      digitalWrite(servoLED2_PIN2, LOW); 
-    }
+    //RGB auf Blau
+    digitalWrite(rgb_red_PIN1, LOW);
+    digitalWrite(rgb_red_PIN2, LOW);
+    digitalWrite(rgb_green_PIN1, LOW);
+    digitalWrite(rgb_green_PIN2, LOW);
+    digitalWrite(rgb_blue_PIN1, HIGH);
+    digitalWrite(rgb_blue_PIN2, HIGH);
     break;
     
     case(loading):
+    //RGB auf Lila
+    digitalWrite(rgb_red_PIN1, HIGH);
+    digitalWrite(rgb_red_PIN2, HIGH);
+    digitalWrite(rgb_green_PIN1, LOW);
+    digitalWrite(rgb_green_PIN2, LOW);
+    digitalWrite(rgb_blue_PIN1, HIGH);
+    digitalWrite(rgb_blue_PIN2, HIGH);
+    
     //servo leds an
-    digitalWrite(servoLED1_PIN1, HIGH);
-    digitalWrite(servoLED1_PIN2, HIGH);
-    
-    digitalWrite(servoLED2_PIN1, LOW);
-    digitalWrite(servoLED2_PIN2, LOW); 
-    
+    digitalWrite(servoLED1_PIN, HIGH);    
+    digitalWrite(servoLED2_PIN, LOW);    
     break;
 
     case(active):
-    
-
+    //RGB auf weiss
+    digitalWrite(rgb_red_PIN1, HIGH);
+    digitalWrite(rgb_red_PIN2, HIGH);
+    digitalWrite(rgb_green_PIN1, HIGH);
+    digitalWrite(rgb_green_PIN2, HIGH);
+    digitalWrite(rgb_blue_PIN1, HIGH);
+    digitalWrite(rgb_blue_PIN2, HIGH);
     break;
 
     case(blocked):
-    //servo auf position setzen
-
-    
+    //servo auf Neues Lager setzen
+    myServo.attach(servo_PIN);
+    setServo();
+    myServo.detach();
+        
     //RGB auf Rot setzen
-    ShiftPWM.SetRGB(1,255,0,0);    // red
-    ShiftPWM.SetRGB(2,255,0,0);    // red
+    digitalWrite(rgb_red_PIN1, HIGH);
+    digitalWrite(rgb_red_PIN2, HIGH);
+    digitalWrite(rgb_green_PIN1, LOW);
+    digitalWrite(rgb_green_PIN2, LOW);
+    digitalWrite(rgb_blue_PIN1, LOW);
+    digitalWrite(rgb_blue_PIN2, LOW);
 
+    //Servo-LEDs an, SL-LEDs aus
+    digitalWrite(servoLED1_PIN, HIGH);
+    digitalWrite(servoLED2_PIN, LOW);  
+    break;
+
+    default:
+  
+    //RGB auf Grün setzen
+    digitalWrite(rgb_green_PIN1, HIGH);
+    digitalWrite(rgb_green_PIN2, HIGH);
+    digitalWrite(rgb_red_PIN1, LOW);
+    digitalWrite(rgb_red_PIN2, LOW);
+    digitalWrite(rgb_blue_PIN1, LOW);
+    digitalWrite(rgb_blue_PIN2, LOW);
     
     if(newKeeper==Lesath||newKeeper==Neutral||newKeeper==Dunkel)
     {
-      digitalWrite(servoLED1_PIN1, LOW);
-      digitalWrite(servoLED1_PIN2, LOW);
-      digitalWrite(servoLED2_PIN1, HIGH);
-      digitalWrite(servoLED2_PIN2, HIGH); 
+      digitalWrite(servoLED1_PIN, LOW);
+      digitalWrite(servoLED2_PIN, HIGH);
+      //Servo auf SL Lager setzen
+      myServo.attach(servo_PIN);
       switch(newKeeper)
       {
         case(Lesath):
         myServo.write(30);
         break;
+
         case(Neutral):
         myServo.write(90);
-
         break;
+
         case(Dunkel):
         myServo.write(150);
-
         break;        
       }
-    }
-    else
-    {
-      //Servo-LEDs an, SL-LEDs aus
-      digitalWrite(servoLED1_PIN1, HIGH);
-      digitalWrite(servoLED1_PIN2, HIGH);
-      digitalWrite(servoLED2_PIN1, LOW);
-      digitalWrite(servoLED2_PIN2, LOW);
-      setServo();
-    }
-    
-    break;
-
-    default:
-    //RGB auf Grün setzen
-    ShiftPWM.SetRGB(1,0,255,0);    // green
-    ShiftPWM.SetRGB(2,0,255,0);    // green
-
-    
-    if(newKeeper==Lesath||newKeeper==Neutral||newKeeper==Dunkel)
-    {
-      digitalWrite(servoLED1_PIN1, LOW);
-      digitalWrite(servoLED1_PIN2, LOW);
-      digitalWrite(servoLED2_PIN1, HIGH);
-      digitalWrite(servoLED2_PIN2, HIGH);
-    }
-    else
-    {
-      //Servo-LEDs an, SL-LEDs aus
-      digitalWrite(servoLED1_PIN1, HIGH);
-      digitalWrite(servoLED1_PIN2, HIGH);
-      digitalWrite(servoLED2_PIN1, LOW);
-      digitalWrite(servoLED2_PIN2, LOW);      
+      delay(3000);
+      myServo.detach();
     }
    return;     
   }
@@ -549,7 +458,6 @@ void pulse()
   switch(_status)
   {
     case(loading):
-      oneByOne();
       myServo.write(servoValue);
       delay(5);
       servoValue+=servoDirection;
@@ -559,7 +467,6 @@ void pulse()
     break;
 
     case(active):
-    hueShiftAll();
       myServo.write(servoValue);
       delay(5);
       servoValue+=servoDirection;
@@ -567,162 +474,5 @@ void pulse()
         servoDirection*=-1;
   return;
 }
-}
-
-void oneByOne(void){ // Fade in and fade out all outputs one at a time
-  unsigned char brightness;
-  unsigned long fadeTime = 500;
-  unsigned long loopTime = numOutputs*fadeTime*2;
-  unsigned long time = millis()-startTime;
-  unsigned long timer = time%loopTime;
-  unsigned long currentStep = timer%(fadeTime*2);
-
-  int activeLED = timer/(fadeTime*2);
-
-  if(currentStep <= fadeTime ){
-    brightness = currentStep*maxBrightness/fadeTime; ///fading in
-  }
-  else{
-    brightness = maxBrightness-(currentStep-fadeTime)*maxBrightness/fadeTime; ///fading out;
-  }
-  ShiftPWM.SetAll(0);
-  ShiftPWM.SetOne(activeLED, brightness);
-}
-
-void inOutTwoLeds(void){ // Fade in and out 2 outputs at a time
-  unsigned long fadeTime = 500;
-  unsigned long loopTime = numOutputs*fadeTime;
-  unsigned long time = millis()-startTime;
-  unsigned long timer = time%loopTime;
-  unsigned long currentStep = timer%fadeTime;
-
-  int activeLED = timer/fadeTime;
-  unsigned char brightness = currentStep*maxBrightness/fadeTime;
-
-  ShiftPWM.SetAll(0);
-  ShiftPWM.SetOne((activeLED+1)%numOutputs,brightness);
-  ShiftPWM.SetOne(activeLED,maxBrightness-brightness);
-}
-
-void inOutAll(void){  // Fade in all outputs
-  unsigned char brightness;
-  unsigned long fadeTime = 2000;
-  unsigned long time = millis()-startTime;
-  unsigned long currentStep = time%(fadeTime*2);
-
-  if(currentStep <= fadeTime ){
-    brightness = currentStep*maxBrightness/fadeTime; ///fading in
-  }
-  else{
-    brightness = maxBrightness-(currentStep-fadeTime)*maxBrightness/fadeTime; ///fading out;
-  }
-  ShiftPWM.SetAll(brightness);
-}
-
-void alternatingColors(void){ // Alternate LED's in 6 different colors
-  unsigned long holdTime = 2000;
-  unsigned long time = millis()-startTime;
-  unsigned long shift = (time/holdTime)%6;
-  for(unsigned int led=0; led<numRGBLeds; led++){
-    switch((led+shift)%6){
-    case 0:
-      ShiftPWM.SetRGB(led,255,0,0);    // red
-      break;
-    case 1:
-      ShiftPWM.SetRGB(led,0,255,0);    // green
-      break;
-    case 2:
-      ShiftPWM.SetRGB(led,0,0,255);    // blue
-      break;
-    case 3:
-      ShiftPWM.SetRGB(led,255,128,0);  // orange
-      break;
-    case 4:
-      ShiftPWM.SetRGB(led,0,255,255);  // turqoise
-      break;
-    case 5:
-      ShiftPWM.SetRGB(led,255,0,255);  // purple
-      break;
-    }
-  }
-}
-
-void hueShiftAll(void){  // Hue shift all LED's
-  unsigned long cycleTime = 10000;
-  unsigned long time = millis()-startTime;
-  unsigned long hue = (360*time/cycleTime)%360;
-  ShiftPWM.SetAllHSV(hue, 255, 255); 
-}
-
-void randomColors(void){  // Update random LED to random color. Funky!
-  unsigned long updateDelay = 100;
-  static unsigned long previousUpdateTime;
-  if(millis()-previousUpdateTime > updateDelay){
-    previousUpdateTime = millis();
-    ShiftPWM.SetHSV(random(numRGBLeds),random(360),255,255);
-  }
-}
-
-void fakeVuMeter(void){ // imitate a VU meter
-  static unsigned int peak = 0;
-  static unsigned int prevPeak = 0;
-  static unsigned long currentLevel = 0;
-  static unsigned long fadeStartTime = startTime;
-  
-  unsigned long fadeTime = (currentLevel*2);// go slower near the top
-
-  unsigned long time = millis()-fadeStartTime;
-  currentLevel = time%(fadeTime);
-
-  if(currentLevel==peak){
-    // get a new peak value
-    prevPeak = peak;
-    while(abs(peak-prevPeak)<5){
-      peak =  random(numRGBLeds); // pick a new peak value that differs at least 5 from previous peak
-    }
-  }
-
-  if(millis() - fadeStartTime > fadeTime){
-    fadeStartTime = millis();
-    if(currentLevel<peak){ //fading in
-      currentLevel++;
-    }
-    else{ //fading out
-      currentLevel--;
-    }
-  }
-  // animate to new top
-  for(unsigned int led=0;led<numRGBLeds;led++){
-    if(led<currentLevel){
-      int hue = (numRGBLeds-1-led)*120/numRGBLeds; // From green to red
-      ShiftPWM.SetHSV(led,hue,255,255); 
-    }
-    else if(led==currentLevel){
-      int hue = (numRGBLeds-1-led)*120/numRGBLeds; // From green to red
-      int value;
-      if(currentLevel<peak){ //fading in        
-        value = time*255/fadeTime;
-      }
-      else{ //fading out
-        value = 255-time*255/fadeTime;
-      }
-      ShiftPWM.SetHSV(led,hue,255,value);       
-    }
-    else{
-      ShiftPWM.SetRGB(led,0,0,0);
-    }
-  }
-}
-
-void rgbLedRainbow(unsigned long cycleTime, int rainbowWidth){
-  // Displays a rainbow spread over a few LED's (numRGBLeds), which shifts in hue. 
-  // The rainbow can be wider then the real number of LED's.
-  unsigned long time = millis()-startTime;
-  unsigned long colorShift = (360*time/cycleTime)%360; // this color shift is like the hue slider in Photoshop.
-
-  for(unsigned int led=0;led<numRGBLeds;led++){ // loop over all LED's
-    int hue = ((led)*360/(rainbowWidth-1)+colorShift)%360; // Set hue from 0 to 360 from first to last led and shift the hue
-    ShiftPWM.SetHSV(led, hue, 255, 255); // write the HSV values, with saturation and value at maximum
-  }
 }
 
